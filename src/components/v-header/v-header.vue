d6b3a475f23061c558aa587667c50fb567f5ade6
ZFlyRay <cuitzfr@hotmail.com>
2018-01-18 11:16:39 +0800

    upload code

---
+++ src/components/v-header/v-header.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="v-header">
+    header
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+  export default {};
+</script>
+
+<style scoped lang="stylus" type="text/stylus" rel="stylesheet/stylus">
+
+</style>
