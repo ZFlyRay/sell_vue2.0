910c1c3477f72ef2cb5d8376c64ba1f40c3e4a27
ZFlyRay <cuitzfr@hotmail.com>
2018-01-18 21:51:41 +0800

    test03

--- src/components/ratings/ratings.vue
+++ src/components/ratings/ratings.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="ratings">
-    ratings
+    ratings add
   </div>
 </template>
 
--- src/components/seller/seller.vue
+++ src/components/seller/seller.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="seller">
-    seller
+    seller add
   </div>
 </template>
 
