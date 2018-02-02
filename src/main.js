906128677a705195b1e954d4eec4ce4341606c9e
ZFlyRay <cuitzfr@hotmail.com>
2018-02-02 09:35:33 +0800

    update image to file v-header

--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
+import 'babel-polyfill';
 import Vue from 'vue';
+import VueResource from 'vue-resource';
 import App from './App';
 import router from './router/index';
 
 import 'common/sass/index.sass';
+
+Vue.use(VueResource);
 
 /* eslint-disable no-new */
 new Vue({
