38f306e4ae120bf2ea2f5da0703a1214965418fa
ZFlyRay <cuitzfr@hotmail.com>
2018-02-04 12:00:32 +0800

    add plugin fastclick
    add plugin fastclick

--- src/main.js
+++ src/main.js
@@ -1,10 +1,13 @@
 import 'babel-polyfill';
+import fastClick from 'fastclick';
 import Vue from 'vue';
 import VueResource from 'vue-resource';
 import App from './App';
 import router from './router/index';
 
 import 'common/sass/index.sass';
+
+fastClick.attach(document.body);
 
 Vue.use(VueResource);
 
