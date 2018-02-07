3c331ee124c88a0e34f852d429db68fd9bb46d72
ZFlyRay <cuitzfr@hotmail.com>
2018-02-07 22:35:51 +0800

    add base components 'scroll'

---
+++ src/base/scroll/scroll.vue
@@ -0,0 +1,90 @@
+<template>
+  <div ref="wrapper">
+    <slot></slot>
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+  import BScroll from 'better-scroll';
+
+  const DIRECTION_H = 'horizontal';
+  const DIRECTION_V = 'vertical';
+
+  export default {
+    props: {
+      probeType: {
+        type: Number,
+        default: 1
+      },
+      click: {
+        type: Boolean,
+        default: false
+      },
+      listenScroll: {
+        type: Boolean,
+        default: false
+      },
+      data: {
+        type: Array,
+        default: null
+      },
+      direction: {
+        type: String,
+        default: DIRECTION_V
+      },
+      refreshDelay: {
+        type: Number,
+        default: 20
+      }
+    },
+    mounted() {
+      setTimeout(() => {
+        this._initScroll();
+      }, 20);
+    },
+    methods: {
+      _initScroll() {
+        if (!this.$refs.wrapper) {
+          return;
+        }
+        this.scroll = new BScroll(this.$refs.wrapper, {
+          probeType: this.probeType,
+          click: this.click,
+          eventPassthrough: this.direction === DIRECTION_V ? DIRECTION_H : DIRECTION_V
+        });
+
+        if (this.listenScroll) {
+          this.scroll.on('scroll', (pos) => {
+            this.$emit('scroll', pos);
+          });
+        }
+      },
+      disable() {
+        this.scroll && this.scroll.disable();
+      },
+      enable() {
+        this.scroll && this.scroll.enable();
+      },
+      refresh() {
+        this.scroll && this.scroll.refresh();
+      },
+      scrollTo() {
+        this.scroll && this.scroll.scrollTo.apply(this.scroll, arguments);
+      },
+      scrollToElement() {
+        this.scroll && this.scroll.scrollToElement.apply(this.scroll, arguments);
+      }
+    },
+    watch: {
+      data() {
+        setTimeout(() => {
+          this.refresh();
+        }, this.refreshDelay);
+      }
+    }
+  };
+</script>
+
+<style scoped lang="sass" type="text/sass" rel="stylesheet/sass">
+
+</style>
