b4a57bbd98975e932b15afea815809e002553a02
ZFlyRay <cuitzfr@hotmail.com>
2018-02-08 21:21:02 +0800

    animation cart-decrease

--- src/base/cartcontrol/cartcontrol.vue
+++ src/base/cartcontrol/cartcontrol.vue
@@ -1,6 +1,10 @@
 <template>
   <div class="cartcontrol">
-    <div class="cart-decrease icon-remove_circle_outline" v-show="food.count>0" @click="decreaseCart"></div>
+    <transition name="move">
+      <div class="cart-decrease" v-show="food.count>0" @click="decreaseCart">
+        <span class="inner icon-remove_circle_outline"></span>
+      </div>
+    </transition>
     <div class="cart-count" v-show="food.count>0">{{food.count}}</div>
     <div class="cart-add icon-add_circle" @click.stop="addCart"></div>
   </div>
@@ -37,13 +41,24 @@
 <style scoped lang="sass" type="text/sass" rel="stylesheet/sass">
   .cartcontrol
     font-size: 0
-    .cart-decrease, .cart-add
+    .cart-decrease
       display: inline-block
       padding: 6px
-      line-height: 24px
-      font-size: 24px
       color: rgb(0, 160, 220)
-    .cart-decrease
+      opacity: 1
+      .inner
+        display: inline-block
+        line-height: 24px
+        font-size: 24px
+        transform: rotate(0)
+        transition: all .4s linear
+      &.move-enter-active, &.move-leave-active
+        transition: all .4s linear
+      &.move-enter, &.move-leave-to
+        transform: translate3d(24px, 0, 0)
+        opacity: 0
+        .inner
+          transform: rotate(180deg)
     .cart-count
       display: inline-block
       vertical-align: top
@@ -55,4 +70,8 @@
       color: rgb(147, 153, 159)
     .cart-add
       display: inline-block
+      padding: 6px
+      line-height: 24px
+      font-size: 24px
+      color: rgb(0, 160, 220)
 </style>
