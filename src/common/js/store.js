80b551b7a690488cc5d99ec2cd70a3b4b1b22665
ZFlyRay <cuitzfr@hotmail.com>
2018-03-27 18:50:36 +0800

    localStorage Function

---
+++ src/common/js/store.js
@@ -0,0 +1,32 @@
+/**
+ * @param id 商家id
+ * @param key
+ * @param val
+ */
+export function saveToLocal(id, key, val) {
+  let seller = window.localStorage.__seller__;
+  if (!seller) { // 第一次存储
+    seller = {};
+    seller[id] = {};
+  } else {
+    seller = JSON.parse(seller);
+    if (!seller[id]) {
+      seller[id] = {};
+    }
+  }
+  seller[id][key] = val;
+  window.localStorage.__seller__ = JSON.stringify(seller);
+};
+
+export function loadFromLocal(id, key, def) {
+  let seller = window.localStorage.__seller__;
+  if (!seller) {
+    return def;
+  }
+  seller = JSON.parse(seller)[id];
+  if (!seller) {
+    return def;
+  }
+  let ret = seller[key];
+  return ret || def;
+};
